8cb905d7f581cc085e19e6e1b61f350b48d49c9c
Antonia Dimitrova <asdimitrova88@gmail.com>
2017-07-09 19:26:10 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+/**
+ * Created by Ani on 9.7.2017 г..
+ */
+'use strict';
+
+var gulp       = require('gulp'),
+    sass       = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify     = require('gulp-uglify'),
+    rename     = require('gulp-rename'),
+    livereload = require('gulp-livereload'),
+    notify = require('gulp-notify'),
+    cssnano = require('gulp-cssnano');
+
+gulp.task('styles', function () {
+    return gulp.src('prototype/scss/**/*.scss')
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(cssnano())
+        .pipe(gulp.dest('assets/css'))
+        .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('scripts', function () {
+    gulp.src('prototype/js/**/*.js')
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('assets/js'))
+        .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts');
+});
+
+// Watch
+gulp.task('watch', function() {
+
+    // Watch .scss files
+    gulp.watch('prototype/scss/**/*.scss', ['styles']);
+
+    // Watch .js files
+    gulp.watch('prototype/js/**/*.js', ['scripts']);
+
+    // Create LiveReload server
+    livereload.listen();
+
+    // Watch any files in dist/, reload on change
+    gulp.watch(['assets/**']).on('change', livereload.changed);
+});
